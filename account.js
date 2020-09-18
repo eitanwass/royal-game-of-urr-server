13db7fa1b7fb8d7dfa9379ba1d02117dea8eaf76
Ethan <eitanwass@gmail.com>
2020-09-18 13:44:39 +0300

    get db credentials from env vars

--- account.js
+++ account.js
@@ -6,10 +6,10 @@ const {removeFromQueue} = require('./matchQueuing');
 const connectedUsers = [];
 
 const mysqlConfig = {
-    host: "sql2.freesqldatabase.com",
-    user: "sql2364384",
-    password: "lI7!aA7*",
-    database: "sql2364384"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 };
 
 exports.account_actions = (socket) => {
